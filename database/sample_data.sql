b7d8bebeef2116f17351dcad61043a2189b50fd9
Harshita Goel <167065803+Erharshita-cloud@users.noreply.github.com>
2026-07-08 00:45:30 +0530

    Create sample_data.sql

---
+++ database/sample_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO bikes (name, price, description) VALUES
+('Napoleon Classic 350', 189999.00, 'Classic cruiser motorcycle'),
+('Napoleon Street 500', 249999.00, 'Street performance bike'),
+('Napoleon Adventure X', 319999.00, 'Adventure touring motorcycle'),
+('Napoleon Sport R', 289999.00, 'Sport motorcycle');
