c2fa394571e29111c9c96535c45a841275a8ebe7
Harshita Goel <167065803+Erharshita-cloud@users.noreply.github.com>
2026-07-20 23:03:57 +0530

    Update scripts.php

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,7 +1,22 @@
-<script
-src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js">
-</script>
+<?php
+declare(strict_types=1);
+
+/**
+ * ---------------------------------------------------------
+ * Napoleon Bikes Platform
+ * Common JavaScript Files
+ * ---------------------------------------------------------
+ */
+?>
+
+<!-- Swiper JS -->
 
 <script
-src="<?= JS ?>app.js">
+    src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js">
+</script>
+
+<!-- Main JavaScript -->
+
+<script
+    src="<?= JS ?>app.js">
 </script>
