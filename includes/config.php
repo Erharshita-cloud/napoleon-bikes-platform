5f86e0fec85cd80a5dc74e6dc9bfa350bf20c730
Harshita Goel <167065803+Erharshita-cloud@users.noreply.github.com>
2026-07-20 17:36:36 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,1 +1,84 @@
-this is config.php file.
+<?php
+declare(strict_types=1);
+
+/*
+|--------------------------------------------------------------------------
+| Napoleon Bikes Configuration
+|--------------------------------------------------------------------------
+*/
+
+define('SITE_NAME', 'Napoleon Bikes');
+define('SITE_TAGLINE', 'Ride Beyond Limits');
+define('SITE_DESCRIPTION', 'Premium motorcycles engineered for every journey.');
+define('SITE_KEYWORDS', 'motorcycles,bikes,sports bike,electric bikes,napoleon bikes');
+define('SITE_AUTHOR', 'Napoleon Bikes');
+
+/*
+|--------------------------------------------------------------------------
+| Base URL
+|--------------------------------------------------------------------------
+| Change this when deploying
+*/
+
+define('BASE_URL', '/');
+
+/*
+|--------------------------------------------------------------------------
+| Assets
+|--------------------------------------------------------------------------
+*/
+
+define('ASSETS', BASE_URL . 'assets/');
+define('CSS', ASSETS . 'css/');
+define('JS', ASSETS . 'js/');
+define('IMG', ASSETS . 'images/');
+
+/*
+|--------------------------------------------------------------------------
+| Company
+|--------------------------------------------------------------------------
+*/
+
+define('COMPANY_EMAIL', 'info@napoleonbikes.com');
+define('COMPANY_PHONE', '+91-9876543210');
+
+define('COMPANY_ADDRESS',
+'Meerut, Uttar Pradesh, India');
+
+/*
+|--------------------------------------------------------------------------
+| Social Links
+|--------------------------------------------------------------------------
+*/
+
+$social = [
+
+'facebook' => '#',
+
+'instagram' => '#',
+
+'linkedin' => '#',
+
+'youtube' => '#'
+
+];
+
+/*
+|--------------------------------------------------------------------------
+| Navigation
+|--------------------------------------------------------------------------
+*/
+
+$navigation = [
+
+'Home' => BASE_URL,
+
+'Bikes' => BASE_URL . 'bikes/',
+
+'Booking' => BASE_URL . 'booking/',
+
+'Pricing' => BASE_URL . 'pricing/',
+
+'Contact' => BASE_URL . 'contact/'
+
+];
