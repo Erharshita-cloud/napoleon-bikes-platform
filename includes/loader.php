ce2f5097e7cf6e4e722b872f68abc1df59dbe7aa
Harshita Goel <167065803+Erharshita-cloud@users.noreply.github.com>
2026-07-20 17:37:37 +0530

    Add loading spinner HTML to loader.php

--- includes/loader.php
+++ includes/loader.php
@@ -1,1 +1,21 @@
-this is loader.php file.
+<div id="loader">
+
+<div class="loader-wrapper">
+
+<img
+
+src="<?= IMG ?>logo.png"
+
+alt="<?= SITE_NAME; ?>">
+
+<div class="spinner"></div>
+
+<p>
+
+Loading...
+
+</p>
+
+</div>
+
+</div>
