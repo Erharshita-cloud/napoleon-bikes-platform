28b2e975a31fc3066e8ad2414c7084b1698c12c6
Harshita Goel <167065803+Erharshita-cloud@users.noreply.github.com>
2026-07-20 17:39:16 +0530

    Add footer section with contact information

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,71 @@
+<footer>
 
+<div class="container">
+
+<div class="footer-logo">
+
+<img
+
+src="<?= IMG ?>logo.png"
+
+alt="<?= SITE_NAME; ?>">
+
+<h2>
+
+<?= SITE_NAME; ?>
+
+</h2>
+
+<p>
+
+<?= SITE_TAGLINE; ?>
+
+</p>
+
+</div>
+
+<div class="footer-contact">
+
+<h3>
+
+Contact
+
+</h3>
+
+<p>
+
+<?= COMPANY_ADDRESS; ?>
+
+</p>
+
+<p>
+
+<?= COMPANY_PHONE; ?>
+
+</p>
+
+<p>
+
+<?= COMPANY_EMAIL; ?>
+
+</p>
+
+</div>
+
+<div class="footer-bottom">
+
+<p>
+
+© <?= date('Y'); ?>
+
+<?= SITE_NAME; ?>
+
+All Rights Reserved.
+
+</p>
+
+</div>
+
+</div>
+
+</footer>
